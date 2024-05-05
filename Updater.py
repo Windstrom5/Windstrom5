de6eb68a2965ff3e94dca4cc6594bf3a08dd0078
Nakashima Angga <111828093+Windstrom5@users.noreply.github.com>
2024-05-06 00:59:53 +0700

    Update Updater.py

--- Updater.py
+++ Updater.py
@@ -3,8 +3,8 @@ import subprocess
 from typing import Tuple
 from datetime import date
 
-def generate_card() -> Tuple[str, str]:
-    COMMAND_ARGS = ["node", "generate.js"]
+def discord() -> Tuple[str, str]:
+    COMMAND_ARGS = ["node", "DiscordUpdater.js"]
     p = subprocess.Popen(args=COMMAND_ARGS, stdout=subprocess.PIPE)
     print(p.communicate()[0].decode())
     p.stdout.close()
